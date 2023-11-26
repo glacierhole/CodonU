85919681519ff3a636dcc91db15d6b887fb09ff0
glacierhole <1761883025@qq.com>
2023-11-26 16:58:02 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import requests
-from PIL import Image
 import io
+from PIL import Image
+import time
 
-def _max_width_(prcnt_width: int = 75):
+def _max_width_(prcnt_width:int = 75):
     max_width_str = f"max-width: {prcnt_width}rem;"
     st.markdown(f""" 
                 <style> 
@@ -14,17 +15,24 @@ def _max_width_(prcnt_width: int = 75):
     )
 _max_width_(80)
 
-# 使用 st.button 的返回值检测按钮是否被点击
-if st.button('点击找到R送你的一张风景图'):
-    # 获取图像数据
-    image_data = requests.get('https://api.gumengya.com/Api/FjImg?format=image').content
+st.button('Fetch Landscape wallpaper')
 
-    # 尝试用 PIL 打开图像
-    try:
-        pil_image = Image.open(io.BytesIO(image_data))
-        
-        # 在容器中显示图像
-        st.image(pil_image, caption='风景图', use_column_width=True)
-    except Exception as e:
-        # 打印异常信息
-        st.error(f"Error opening image: {e}")
+
+def getImageLink():
+    return 'https://api.gumengya.com/Api/FjImg?format=image'
+
+def getImage(url):
+    return requests.get(url).content
+
+def getDogeBytesIO():
+    container = io.BytesIO()
+    container.write(getImage(getImageLink()))
+    return container
+
+loading_bar = st.progress(0.0, '加载中')
+time.sleep(0.1)
+loading_bar.progress(0.1, '加载中')
+image = getDogeBytesIO()
+loading_bar.progress(1.0, '加载完成')
+time.sleep(0.1)
+loading_bar.image(image)

f2a51a3e249a4507f6a40c1a149bb8d1152f7579
glacierhole <1761883025@qq.com>
2023-11-26 18:10:25 +0800

    Update test.py

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ def main():
     st.write("作者：饶一率")
     st.write("时间：2023-09-16")
     st.write("""## 物种信息""")
-    st.write(f"{suzhu}基因组编码基因来源：[NCBI]({suzhuweb})")
+st.write(f"{suzhu}基因组编码基因来源：[NCBI]({suzhuweb})")
 st.write(f"选择GCA_001661405.1来进行统计得到{suzhu}密码子打分表")
 st.sidebar.image('picture/ulogo.png', use_column_width=True)
 st.sidebar.write("""## 序列上传方式1""")
